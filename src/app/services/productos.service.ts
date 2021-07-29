c336ba985a08762c342d179c2f3d334ccce1f8d3
Raul Martinez <raul231199@gmail.com>
2021-07-29 16:41:47 -0500

    Fin sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,17 +9,56 @@ export class ProductosService {
 
   cargando: boolean = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-9727c-default-rtdb.firebaseio.com/productos_idx.json')
+
+    return new Promise( (resolve: any, reject) => {
+      this.http.get('https://angular-html-9727c-default-rtdb.firebaseio.com/productos_idx.json')
       .subscribe( ( resp: any) => {
-        console.log(resp);
         this.productos = resp;
         this.cargando = false;
+        resolve();
       });
+    });
+  }
+
+  public getProducto(id: String) {
+    return this.http.get(`https://angular-html-9727c-default-rtdb.firebaseio.com/productos/${ id }.json`);
+  }
+
+  public buscarProducto(termino: string) {
+
+    if (this.productos.length == 0) {
+      //Cargar productos
+      this.cargarProductos().then(() => {
+        //Ejecutat despues de tener los productos
+        //Aplicar el filtro
+        this.filtrarProductos(termino);
+      });
+    } else {
+      //Aplicar el filtro
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos(termino: string) {
+    //console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLowerCase();
+
+      if(prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0 ) {
+        this.productosFiltrado.push(prod);
+      }
+    });
   }
 }
